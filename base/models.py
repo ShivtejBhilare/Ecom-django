6586be995b318f92f402254c071509d6e2828745
Shivtej Bhilare <bhilareshivtej@gmail.com>
2024-03-07 20:35:34 +0530

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.db import models
+class BaseModel(models.Model):
+    uid=models.models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_add=True)
+    class Meta:
+        abstract=True
